73de5625388674d69be8ec7b372501ea524fa11e
Puneeth Reddy H C <puneethreddy951@gmail.com>
2019-11-03 11:41:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 class DefaultConfigs(object):
     #1.string parameters
-    train_data = "PlantVillage/train/"
-    test_data = "PlantVillage/val/"
+    train_data = "/kaggle/input/plant-diseases/dataset_itr2/dataset_itr2/train/"
+    test_data = "/kaggle/input/plant-diseases/dataset_itr2/dataset_itr2/test/"
     val_data = "no"
     model_name = "resnet50"
     weights = "./checkpoints/"
     best_models = weights + "best_model/"
     submit = "./submit/"
     logs = "./logs/"
-    gpus = "0"
+    gpus = "3"
 
     #2.numeric parameters
     epochs = 40
